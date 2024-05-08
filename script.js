b1d727cd285d61a6163892846bc7a8fdcbcc5107
Kevin21 <kevu4776@gmail.com>
2024-05-08 01:29:59 -0500

    created variables to store the first and second variables along with the operator. Added skeleton for a function that runs all the calculations. Started creating visuals for the calculator

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+var first;
+var operator;
+var second;
 
 var buttonX = document.querySelector("#x");
 buttonX.addEventListener('click', function(){
     console.log(multiply(2, 3));
 });
+
+function operate(first, operator, second){
+    if (operator === '+'){
+        //call add function
+    }
+    else if(operator === '-'){
+        //call subtract function
+    }
+    else if(operator === 'x'){
+        //call multiply function
+    }
+    else if(operator === '/'){
+        //call divide function
+    }
+}
